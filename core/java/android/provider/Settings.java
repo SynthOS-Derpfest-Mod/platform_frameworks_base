d72493df5b8b3e538ca622fdb1acdfc14b0f4189
TikkiTikki <radstylemusic@gmail.com>
2020-04-12 23:06:26 -0300

    Hide Things below VolumePanel [1/2]
    by.tikkiX2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6473,6 +6473,12 @@ public final class Settings {
           */
          public static final String SYNTHOS_LOCK_COOL_DIVIDER_SHOW = "synthos_lock_cool_divider_show";
 
+          /**
+           * Whether show/hide things in volume panel by.tikkiX2
+           * @hide
+           */
+          public static final String SYNTHOS_HIDE_THINGS_VOLUMEPANEL = "synthos_hide_things_volumepanel";
+
         /**
          * Color of the weather condition icons on the lock screen
          * @hide
