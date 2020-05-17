aa0c5829b85927f5a31674d0e7e0745be6750457
TikkiTikki <radstylemusic@gmail.com>
2020-05-17 06:10:15 -0300

    Add VolumePanel Styles [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6570,6 +6570,14 @@ public final class Settings {
         */
         public static final String SYNTHOS_VISUALIZER_QSPANEL = "synthos_visualizer_qspanel";
 
+
+        /**
+        * VolumePanel Themes
+        * by.tikkiX2
+        * @hide
+        */
+        public static final String SYNTHOS_VOLUME_PANEL_THEME = "synthos_volume_panel_theme";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
