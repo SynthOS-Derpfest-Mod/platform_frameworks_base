8ef3b32d207f472723ac64dc3a348fedc5ba63d4
TikkiTikki <radstylemusic@gmail.com>
2020-05-23 23:11:38 -0300

    Add MusicText next to VolumePanel [1/2]
    Add a text next to the volume panel that shows the Media information. This can probably be improved, this is done unexpectedly, so please excuse what is left over.
    SynthOS - a DerpFest mod
    by.tikkiX2

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1405,6 +1405,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         // Other icons
         mVolumeComponent = getComponent(VolumeComponent.class);
+        mVolumeComponent.initDependencies(mMediaManager);
 
         mNotificationPanel.setUserSetupComplete(mUserSetup);
         if (UserManager.get(mContext).isUserSwitcherEnabled()) {
