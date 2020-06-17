a4cecdbd7918e0e79d9cc97daac8cc949bb15767
TikkiX2 <radstylemusic@gmail.com>
2020-06-16 17:46:42 -0700

    Add Iota

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -123,6 +123,7 @@ import dagger.Subcomponent;
 
 import com.google.android.systemui.AssistManagerGoogle;
 
+import com.android.prophet.iota.Iota;
 import com.android.prophet.gamma.Gamma;
 
 /**
@@ -301,6 +302,7 @@ public class Dependency {
     @Inject Lazy<INotificationManager> mINotificationManager;
     @Inject Lazy<FalsingManager> mFalsingManager;
     @Inject Lazy<PulseController> mPulseController;
+    @Inject Lazy<Iota> mIota;
     @Inject Lazy<Gamma> mGamma;
 
     @Inject
@@ -491,6 +493,7 @@ public class Dependency {
         mProviders.put(INotificationManager.class, mINotificationManager::get);
         mProviders.put(FalsingManager.class, mFalsingManager::get);
         mProviders.put(PulseController.class, mPulseController::get);
+        mProviders.put(Iota.class, mIota::get);
         mProviders.put(Gamma.class, mGamma::get);
 
         // TODO(b/118592525): to support multi-display , we start to add something which is
